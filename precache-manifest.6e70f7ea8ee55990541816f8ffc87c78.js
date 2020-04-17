2b6bbf4507cc328ffa47884bc15ae11d239688d7
Jean luc <luctunechi45@gmail.com>
2020-04-17 23:31:43 +0200

    Updates

---
+++ precache-manifest.6e70f7ea8ee55990541816f8ffc87c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e536b7926cc581c23ccc41c2a3d0c84c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83934162a78f153a91f1",
+    "url": "/monsters-rolodex/static/css/main.bd52ad25.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83934162a78f153a91f1",
+    "url": "/monsters-rolodex/static/js/main.db6e34e6.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
